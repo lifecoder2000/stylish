f381d8cadaba3d8f8b9a64a9e4e1f924269b316e
Taehyuny <hyuny6012@naver.com>
2018-08-16 14:01:37 +0900

    ajax로 필터링 구현 및 장바구니 버튼 설명 추가, 장바구니에 담을 때 Choose an option은생성 안되게 만듬.

--- routes/index.js
+++ routes/index.js
@@ -12,34 +12,48 @@ router.get('/', async(req, res) => {
 });
 
 router.get('/product', async(req, res) => {
-    let products = await Products.find();
-    if(require('../config/status').isBlocked){ return res.render('serverChecking'); }
-    else{ return res.render('product', {user_id : req.session.user_id, products : products}); }
-});
-
-router.post('/product', async(req, res) => {
-    if(req.body.sorting == "DefaultSorting"){
+    if(req.param('sorting') == "DefaultSorting"){
         let products = await Products.find();
-        return res.render('product', {user_id : req.session.user_id, products : products});
+        if(require('../config/status').isBlocked){ return res.render('serverChecking'); }
+        else{ return res.render('product', {user_id : req.session.user_id, products : products}); }
     }
-    if(req.body.sorting == "Popularity"){
+    else if(req.param('sorting') == "Popularity"){
         let products = await Products.find().sort({purchaseAmount:-1});
         return res.render('product', {user_id : req.session.user_id, products : products});
     }
-    if(req.body.sorting == "Price_lowtohigh"){
+    else if(req.param('sorting') == "Price_lowtohigh"){
         let products = await Products.find().sort({price:1});
         return res.render('product', {user_id : req.session.user_id, products : products});
     }
-    if(req.body.sorting == "Price_hightolow"){
+    else if(req.param('sorting') == "Price_hightolow"){
         let products = await Products.find().sort({price:-1});
         return res.render('product', {user_id : req.session.user_id, products : products});
+    }else{
+        let products = await Products.find();
+        if(require('../config/status').isBlocked){ return res.render('serverChecking'); }
+        else{ return res.render('product', {user_id : req.session.user_id, products : products}); }
+    }
+});
+
+router.post('/product', async(req, res) => {
+    if(req.body.sortName == "DefaultSorting"){
+        return res.send({result : true, path : `/product?sorting=DefaultSorting`});
+    }
+    if(req.body.sortName == "Popularity"){
+        return res.send({result : true, path : `/product?sorting=Popularity`});
+    }
+    if(req.body.sortName == "Price_lowtohigh"){
+        return res.send({result : true, path : `/product?&sorting=Price_lowtohigh`});
+    }
+    if(req.body.sortName == "Price_hightolow"){
+        return res.send({result : true, path : `/product?&sorting=Price_hightolow`});
     }
 });
 
 router.get('/product-detail', async(req, res) => {
     let products = await Products.find();
     if(require('../config/status').isBlocked){ return res.render('serverChecking'); }
-    else{ return res.render('product-detail', { products : products, productName : req.param('name'), productPrice : req.param('price')}); }
+    else{ return res.render('product-detail', { user_id : req.session.user_id, products : products, productName : req.param('name'), productPrice : req.param('price')}); }
 });
 
 router.get('/cart', async(req, res) => {
@@ -111,6 +125,26 @@ router.get('/logout', (req, res) => {
 
 /* 카트 추가 */
 router.post('/cart', async(req, res) => {
+    let findProduct = await Products.findOne({ name : req.body.productName });
+    if(req.body.productSize == "Choose an option" || req.body.productColor == "Choose an option"){ // 기본 Choose an option은 옵션으로 선택 불가
+        return res.send(`<script>alert('옵션을 모두 선택해주세요 !');</script>`); // 왜 작동 안돼지?
+        // return res.send({ result : true, path : `/product-detail?name=${findProduct.name}&price=${findProduct.price}`});
+    }else{
+        try{
+            await ShoppingBasket.create({
+                userId : req.session.user_id,
+                productNamekey : req.body.productName,
+                products : {
+                    productName : req.body.productName,
+                    productPrice : req.body.productPrice,
+                    productSize : req.body.productSize,
+                    productColor : req.body.productColor,
+                    productCount : req.body.productCount
+                }    
+            });
+        }
+        catch(err){ return res.send(`<script>alert('오류가 발생했습니다.');location.href='/product';</script>`); }
+    }
     /*
         { 
         productName: 'stylish long T-shirt',
@@ -120,20 +154,7 @@ router.post('/cart', async(req, res) => {
         productCount: '2' 
         }
     */
-   try{
-        await ShoppingBasket.create({
-            userId : req.session.user_id,
-            productNamekey : req.body.productName,
-            products : {
-                productName : req.body.productName,
-                productPrice : req.body.productPrice,
-                productSize : req.body.productSize,
-                productColor : req.body.productColor,
-                productCount : req.body.productCount
-            }    
-        });
-    }
-    catch(err){ return res.send(`<script>alert('오류가 발생했습니다.');location.href='/product';</script>`); }
+   
 });
 
 router.post('/cart/deleteOne', async(req, res) => {
