1382fdb75a1e04803de99ace77fdb5d265da35bd
Taehyuny <hyuny6012@naver.com>
2018-08-20 03:34:45 +0900

    결제 모든 상태 업데이트 구현

--- routes/users.js
+++ routes/users.js
@@ -36,7 +36,47 @@ router.get('/mypage/basket', async(req, res) => {
 /* 주문내역(주문내역(주문하기, 주문취소), 배송 조회,  교환&반품 신청) */
 router.get('/mypage/Ordered', async(req, res) => {
     let findPaymentBasket = await PaymentBasket.find({userId : req.session.user_id});
-    if(findPaymentBasket){ res.render('ordered', { PaymentBasket : findPaymentBasket, userId : req.session.user_id}); }
+    if(findPaymentBasket){ 
+        res.render('ordered', { PaymentBasket : findPaymentBasket, userId : req.session.user_id});
+     }
+});
+
+router.post('/mypage/Ordered/cancelOrder',async(req,res) => {
+    let findPaymentBasket = await PaymentBasket.findOne({_id : req.body._id});
+    if(findPaymentBasket){
+        await PaymentBasket.updateOne({_id : req.body._id},{status : "주문취소"});
+        return res.send(`<script>alert('주문 취소가 되었습니다 :)');location.href='/user/mypage/Ordered';</script>`);
+    }
+});
+
+
+router.get('/mypage/Ordered/takeBack', async(req,res) => {
+    let findPaymentBasket = await PaymentBasket.findOne({_id : req.param('_id')});
+    if(findPaymentBasket){
+        if(req.param('choice') == "return"){
+            await PaymentBasket.updateOne({_id : req.param('_id')},{status : "환불"});
+            return res.send(`<script>alert('환불되었습니다 :)');location.href='/user/mypage/Ordered';</script>`);
+        }
+        if(req.param('choice') == "change"){
+            await PaymentBasket.updateOne({_id : req.param('_id')},{status : "교환신청"});
+            return res.send(`<script>alert('교환 신청되었습니다 :)');location.href='/user/mypage/Ordered';</script>`);
+        }
+    }
+});
+
+router.post('/mypage/Ordered/takeBack', async(req,res) => {
+    let findPaymentBasket = await PaymentBasket.findOne({_id : req.body._id});
+    if(findPaymentBasket){
+        if(req.body.takeBack == "반품신청(환불)"){
+            await PaymentBasket.updateOne({_id : req.param('_id')},{status : "환불"});
+            return res.send(`<script>alert('환불되었습니다 :)');location.href='/user/mypage/Ordered';</script>`);
+        }else{
+            res.send(`<script>let choice = prompt('환불 하시겠습니까? 교환하시겠습니까? (환불 | 교환 입력)');
+                if(choice === "환불"){ location.href='/user/mypage/Ordered/takeBack?choice=return&_id=${req.body._id}'; } 
+                else if(choice === "교환"){ location.href='/user/mypage/Ordered/takeBack?choice=change&_id=${req.body._id}'; }
+                else{ alert("제대로 입력해주세요");location.href='/user/mypage/Ordered'; };</script>`);
+        }
+    }
 });
 
 router.get('/check', (req, res) => {
